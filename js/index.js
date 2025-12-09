5d34f5bffbde0a095a57de228a5dec7bb69150d7
timothyrobinsonblog <53365029+timothyrobinsonblog@users.noreply.github.com>
2025-12-09 18:35:15 +1100

    relink2

--- js/index.js
+++ js/index.js
@@ -2,12 +2,15 @@ document.addEventListener('DOMContentLoaded', () => {
     const btn = document.querySelector('.button');
     const html = document.documentElement;
     const branding = document.querySelector('.branding');
+    const textSymbol = document.querySelector('.text-symbol');
     // const accentPrimary = document.querySelector('.text-accent-primary');
   
     btn.addEventListener('mouseenter', () => {
       html.style.backgroundColor = '#0D1117';
       branding.style.color = '#7D8590';
       branding.style.borderTopColor = '#7D8590';
+      textSymbol.style.color = '#7D8590';
+      textSymbol.style.borderColor = '#7D8590';
     //   accentPrimary.style.color = '#7D8590';
     //   accentPrimary.style.fontFamily = 'Palatino';
     });
@@ -16,6 +19,8 @@ document.addEventListener('DOMContentLoaded', () => {
       html.style.backgroundColor = '';
       branding.style.color = '';
       branding.style.borderTopColor = '';
+      textSymbol.style.color = '';
+      textSymbol.style.borderColor = '';
     //   accentPrimary.style.color = '';
     //   accentPrimary.style.fontFamily = '';
     });
